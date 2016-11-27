2de92373948fb2d8d211972982ca9088a4c1b41e
Harry Hu <hhu@live.ca>
2016-11-27 10:19:19 -0500

    doctor: update 2

--- src/app/util/util.js
+++ src/app/util/util.js
@@ -4,7 +4,7 @@
 app.provider('util', function() {
     this.$get = function() {
         return {
-            baseApiUrl: 'http://localhost:3000/',
+            baseApiUrl: 'http://127.0.0.1:3000/',
             // baseApiUrl: 'http://139.224.68.92:3000/',
 
             getResponse: function(response) {
